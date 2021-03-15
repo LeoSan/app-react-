2fc479323558b0a7722e54b69de2b62f8b93d08b
LeoSan <cuenca623@gmail.com>
2021-03-15 15:13:08 -0600

    Autor: Leonard Cuenca, Desarrollo: Culminaciòn de APP del clima con funcionalidad de pintar interfaz dependiendo la temperatura.

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect, Alert} from 'react';
 import { StyleSheet, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Formulario from './components/Formulario';
+import Resultado from './components/Resultado';
 
 export default function App() {
 //UsaremosEstate para la logica de negocio
@@ -14,8 +15,13 @@ const  [ busqueda, setBusqueda ] = useState({
 
 const [consultar, setConsultar] = useState(false);
 const [resultado, setResultado] = useState({});
+const [bgColor,   setbgColor]   = useState('rgb(71, 149, 212)');
 
 const {ciudad, pais} = busqueda;
+
+const bgColorApp = {
+  backgroundColor: bgColor
+}
 
 //Funciones  App
 
@@ -45,9 +51,16 @@ const consultarAPIClima = async () =>{
           const appId = 'fc26b927fb7c7891b06d8135e92a24b1';
           const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
+          let request = new Request( url, {
+             method:'GET',
+          });  
+
           try {
 
-            const respuesta = await fetch(url);
+            const respuesta = await fetch(request)
+              .catch(function (err){
+                console.error(err);
+              });
             const resultado = await respuesta.json();
 
             console.log(resultado);
@@ -55,9 +68,23 @@ const consultarAPIClima = async () =>{
             //Cambiamos el state para que el useEffect funcione
             setConsultar(false);
 
+            // Modifica los colores de fondo 
+            const kelvin   = 273.15;
+            const { main } = resultado;
+            const actual   = main.temp - kelvin;
+
+
+            if(actual < 10) {
+              setbgColor('rgb( 105, 108, 149 )');
+            } else if(actual >= 10 && actual < 29) {
+              setbgColor('rgb(71, 149, 212)');
+            } else {
+              setbgColor('rgb( 178, 28, 61)');
+            }
+
 
           } catch (error) {
-
+           // console.log("alertas");  
             mostrarAlerta();
 
           }
@@ -72,13 +99,16 @@ const consultarAPIClima = async () =>{
   return (
     <>
       <TouchableWithoutFeedback onPress={ ()=>ocultarTeclado() }>
-          <View style={styles.app}>
+            <View style={[styles.app, bgColorApp ]}>
               <View style={styles.contenido}>
                   <Formulario
                       busqueda={busqueda}
                       setBusqueda={setBusqueda}
                       setConsultar={setConsultar}
 
+                    />
+                    <Resultado
+                        resultado = {resultado}
                     />
               </View>
           </View>
@@ -89,7 +119,6 @@ const consultarAPIClima = async () =>{
 
 const styles = StyleSheet.create({
   app: {
-    backgroundColor: 'rgb(71, 149, 212)',
     flex: 1,
     justifyContent: 'center'
   },
--- components/Resultado.js
+++ components/Resultado.js
@@ -1,21 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function Resultado() {
+export default function Resultado({resultado}) {
+
+
+  const {name, main} = resultado; 
+
+  if (!name ) return null;
+
+  //Formula para grando K a C 
+  const kelvin = 273.15;
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <Text style={[styles.texto, styles.actual]}>
+          { parseInt( main.temp - kelvin )}&#x2103;
+          <Text style={styles.temperatura} >  </Text>
+          <Image 
+                    style={{width: 66, height: 58}}
+                    source={{ uri: `http://openweathermap.org/img/w/${resultado.weather[0].icon}.png` }}
+          />
+      </Text>
+
+      <View style={ styles.temperatura2 }>
+        <Text style={ styles.texto }> Min {''}
+            <Text style={styles.temperatura} >  
+                { parseInt(main.temp_min - kelvin) }&#x2103;
+            </Text>
+        </Text>
+        <Text style={ styles.texto}>Max {''}
+            <Text style={styles.temperatura} >  
+                { parseInt(main.temp_max - kelvin) }&#x2103;
+            </Text>
+        </Text>
+
+      </View>
+
+
+      
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom:20,
+
+  },  
+  texto: {
+    color:'#FFF',
+    fontSize:20,
+    textAlign:'center', 
+    marginRight:20,
+
+  },
+  actual: {
+    fontSize:80,
+    marginRight:0, 
+    fontWeight:'bold', 
+  },  
+  temperatura: {
+    fontSize:24, 
+    fontWeight:'bold', 
+
+  }, 
+  temperatura2: {
+    flexDirection:'row', 
+    justifyContent: 'center', 
   },
 });
